eb669c438ced6fa583c43a9a45155d20539f3a07
Shera Adams <110789514+sheraadams@users.noreply.github.com>
2023-01-05 19:19:09 -0500

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -5,7 +5,15 @@
 #pragma once
 using namespace std;
 class Menu {
+private:
 
+    const int MONTHS = 12;
+
+    float YearEndInterest;
+
+    float principle;
+
+    float interestEarned;
 public:
 
     void pMenu();
