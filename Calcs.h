4cd76c12a26bb63e040b14bf170e02d9520e0d36
Shera Adams <110789514+sheraadams@users.noreply.github.com>
2023-01-05 19:21:45 -0500

    Update Calcs.h

--- Calcs.h
+++ Calcs.h
@@ -5,9 +5,20 @@
 using namespace std;
 
 class Calcs {
+private: 
 
+    const int MONTHS = 12;
+  
+    float YearEndInterest;
+
+    float principle;
+
+    float interestEarned;
+    
 public:
 
+    float account_balance, deposit, interest, years;
+    
     void noDeposits(float account_balance, float deposit,float interest, float years);
 
 };
